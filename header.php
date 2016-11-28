d23302cfced5b6dd6e02c422cd6f36f55dc57dba
Jesper Dalsgaard <post@jdanet.dk>
2016-11-28 11:08:54 +0100

    Indexfil opdelt i header, index og footer

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title></title>
+	<meta name="description" content="" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/custom.css" />
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="da">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<title></title>
-	<meta name="description" content="" />
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href="css/custom.css" />
-</head>
-<body>
+<?php include('header.php') ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 top">1</div>
@@ -16,5 +6,4 @@
 			<div class="col-md-4 top">3</div>
 		</div>
 	</div>
-</body>
-</html>
+<?php include('footer.php') ?>
